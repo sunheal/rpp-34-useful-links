70b3c17e03aef58bca2d41ca7582f5f75719ce55
sunheal <sunheal@gmail.com>
2022-01-25 21:38:51 -0500

    Version 0.0.3

---
+++ README.md
@@ -0,0 +1,4 @@
+# rpp-34-useful-links README
+
+This Extension provides convinient access to RPP34's frequently visited sites. Enjoy it!
+
---
+++ rpp34Sites.js
@@ -0,0 +1,44 @@
+const rpp34Sites = [
+  {
+    label: 'Zoom All',
+    detail: 'Zoom All Link',
+    url: 'https://bit.ly/rpp-zoom'
+  },
+  {
+    label: 'Cohort Zoom',
+    detail: 'RPP34 Cohort Zoom Link',
+    url: 'https://bit.ly/rpp34-zoom'
+  },
+  {
+    label: 'GLearn',
+    detail: 'GLearn Link',
+    url: 'https://learn-2.galvanize.com/cohorts/3025'
+  },
+  {
+    label: 'WhiteBoard',
+    detail: 'RPP34 WhiteBoard Link',
+    url: 'https://docs.google.com/spreadsheets/d/12K3dPbr9RbtR3JKWHAMjKtSBussJYyRTpSJR-bocfC8/edit#gid=1448522408'
+  },
+  {
+    label: 'OICE Form',
+    detail: 'OICE Form Link',
+    url: 'https://bit.ly/rpp34-oice'
+  },
+  {
+    label: 'Cohort Github Repos',
+    detail: 'RPP34 Github Repos Link',
+    url: 'https://github.com/orgs/hackreactor/teams/students-hr-rpp34/repositories'
+  },
+  {
+    label: 'Colloboration Calendar',
+    detail: 'RPP34 Colloboration Calendar Link',
+    url: 'https://calendar.google.com/calendar/embed?src=c_i8n56h04q7gvkl1tci0no8lhdg%40group.calendar.google.com'
+  },
+  {
+    label: 'Toy Probelms',
+    detail: 'RPP34 Toy Probelms Link',
+    url: 'https://github.com/hackreactor/hr-rpp34-toy-problems'
+  },
+];
+
+exports.rpp34Sites = rpp34Sites;
